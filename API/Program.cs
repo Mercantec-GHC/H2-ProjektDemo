f446aa8c0c48e97cb26a88a9e65f68ec5969dfe7
MAGS <120091142+Mercantec-MAGS@users.noreply.github.com>
2024-04-22 10:19:07 +0200

    Added Cors

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,20 @@
 using Npgsql;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("*")
+                                .AllowAnyMethod()
+                                .AllowAnyHeader();
+                      });
+});
 
 // Add services to the container.
 
